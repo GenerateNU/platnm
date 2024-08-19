3dbf31bc970027c34ad91d7acd39e8303e7179bb
ddusichka <78527291+ddusichka@users.noreply.github.com>
2024-08-18 20:00:53 -0400

    add route with param

--- backend/routes/hello.go
+++ backend/routes/hello.go
@@ -5,7 +5,14 @@ import "github.com/gofiber/fiber/v2"
 func HelloRoutes(a *fiber.App) {
 	route := a.Group("/hello")
 
-	route.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, world!")
-	})
+	route.Get("/", helloWorld)
+	route.Get("/:name", helloName)
+}
+
+func helloWorld(c *fiber.Ctx) error {
+	return c.SendString("Hello, world!")
+}
+
+func helloName(c *fiber.Ctx) error {
+	return c.SendString("Hello, " + c.Params("name") + "!")
 }
