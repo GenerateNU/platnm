13c95271ee93c52a74163db3b43fcaebf1c840bf
ddusichka <78527291+ddusichka@users.noreply.github.com>
2024-09-17 15:00:01 -0400

    update ID types and seed data

--- backend/internal/supabase/migrations/20240917002108_base_schema.sql
+++ backend/internal/supabase/migrations/20240917002108_base_schema.sql
@@ -10,52 +10,51 @@ create table if not exists "user" (
 );
 
 create table if not exists artist (
-    id uuid primary key default uuid_generate_v4(),
+    id bigserial primary key,
     name text not null,
     photo text,
     bio text
 );
 
 create table if not exists genre (
-    id uuid primary key default uuid_generate_v4(),
+    id serial primary key,
     name text not null
 );
 
 create table if not exists album (
-    id uuid primary key default uuid_generate_v4(),
+    id bigserial primary key,
     title text not null,
     release_date date not null,
     cover text,
     country text,
-    genre_id uuid references genre(id)
+    genre_id integer references genre(id)
 );
 
 create table if not exists album_artist (
-    album_id uuid references album(id),
-    artist_id uuid references artist(id),
+    album_id bigint references album(id),
+    artist_id bigint references artist(id),
     primary key (album_id, artist_id)
 );
 
 create table if not exists track (
-    id uuid primary key default uuid_generate_v4(),
-    album_id uuid references album(id),
-    artist_id uuid references artist(id),
+    id bigserial primary key,
+    album_id bigint not null references album(id),
     title text not null,
     duration_seconds int not null
 );
 
 create table if not exists track_artist (
-    track_id uuid references track(id),
-    artist_id uuid references artist(id),
+    track_id bigint not null references track(id),
+    artist_id bigint not null references artist(id),
     primary key (track_id, artist_id)
 );
 
 create type media_type as enum ('album', 'track');
 
 create table if not exists review (
-    id uuid primary key default uuid_generate_v4(),
+    id serial primary key,
     user_id uuid not null references "user"(id),
-    media_id uuid not null, -- can't foreign key to album or track because it could be either
+    media_id integer not null, -- can't foreign key to album or track because it could be either
     media_type media_type not null,
     rating int not null,
     comment text,
