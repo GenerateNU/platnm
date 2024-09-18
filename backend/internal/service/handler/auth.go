344a39621a9cc1ea9eb25f16c40ae424728714a4
aaronkimbrooktec <218adk@gmail.com>
2024-09-17 22:59:26 -0400

    refactor: clean up spotify oauth

--- backend/internal/service/handler/auth.go
+++ backend/internal/service/handler/auth.go
@@ -20,6 +20,14 @@ var (
 	SpotifyTokenURL = "https://accounts.spotify.com/api/token"
 )
 
+type TokenResponse struct {
+	AccessToken  string `json:"access_token"`
+	TokenType    string `json:"token_type"`
+	Scope        string `json:"scope"`
+	ExpiresIn    int    `json:"expires_in"`
+	RefreshToken string `json:"refresh_token"`
+}
+
 type CodeVerifierStore struct {
 	CodeVerifierMap map[string]string
 	Mu              sync.Mutex
@@ -50,7 +58,7 @@ func (h *AuthHandler) SpotifyRedirect(c *fiber.Ctx) error {
 	params := url.Values{}
 	params.Add("client_id", os.Getenv("SPOTIFY_CLIENT_ID"))
 	params.Add("response_type", "code")
-	params.Add("redirect_uri", "http://localhost:8080/auth/spotify/callback")
+	params.Add("redirect_uri", os.Getenv("SPOTIFY_REDIRECT_URI"))
 	params.Add("state", state)
 	params.Add("scope", "user-read-private")
 	params.Add("code_challenge_method", "S256")
@@ -83,7 +91,7 @@ func (h *AuthHandler) SpotifyCallback(c *fiber.Ctx) error {
 	params := url.Values{}
 	params.Add("grant_type", "authorization_code")
 	params.Add("code", code)
-	params.Add("redirect_uri", "http://localhost:8080/auth/spotify/callback")
+	params.Add("redirect_uri", os.Getenv("SPOTIFY_REDIRECT_URI"))
 	params.Add("client_id", os.Getenv("SPOTIFY_CLIENT_ID"))
 	params.Add("code_verifier", verifier)
 
@@ -110,14 +118,6 @@ func (h *AuthHandler) SpotifyCallback(c *fiber.Ctx) error {
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": err.Error()})
-	}
-
-	type TokenResponse struct {
-		AccessToken  string `json:"access_token"`
-		TokenType    string `json:"token_type"`
-		Scope        string `json:"scope"`
-		ExpiresIn    int    `json:"expires_in"`
-		RefreshToken string `json:"refresh_token"`
 	}
 
 	var tokenResponse TokenResponse
