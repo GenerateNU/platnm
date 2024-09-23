6d67c7088951e4f910decab3f0a6df586f591953
aaronkimbrooktec <218adk@gmail.com>
2024-09-23 12:15:51 -0400

    fix: make pr changes

--- backend/internal/storage/postgres/schema/review.go
+++ backend/internal/storage/postgres/schema/review.go
@@ -2,14 +2,13 @@ package schema
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"platnm/internal/models"
 
 	"platnm/internal/errs"
 
-	"github.com/jackc/pgerrcode"
 	"github.com/jackc/pgx/v5"
-	"github.com/jackc/pgx/v5/pgconn"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
@@ -18,36 +17,36 @@ type ReviewRepository struct {
 }
 
 const (
-	UserFKeyConstraint = "review_user_id_fkey"
+	userFKeyConstraint        = "review_user_id_fkey"
+	uniqueUserMediaConstraint = "unique_user_media"
 )
 
 func (r *ReviewRepository) CreateReview(ctx context.Context, review *models.Review) (*models.Review, error) {
 	query := `
-	WITH media_exists AS (
-    SELECT CASE
-        WHEN $3 = 'track' AND EXISTS (SELECT 1 FROM track WHERE id = $2) THEN true
-        WHEN $3 = 'album' AND EXISTS (SELECT 1 FROM album WHERE id = $2) THEN true
-        ELSE false
-    END AS exists
+	WITH media_check AS (
+		SELECT 1
+		FROM track
+		WHERE id = $2 AND $3 = 'track'
+		UNION
+		SELECT 1
+		FROM album
+		WHERE id = $2 AND $3 = 'album'
 	)
 	INSERT INTO review (user_id, media_id, media_type, rating, comment)
 	SELECT $1, $2, $3::media_type, $4, $5
-	FROM media_exists
-	WHERE exists
+	FROM media_check
 	RETURNING id;
 	`
 
 	if err := r.QueryRow(ctx, query, review.UserID, review.MediaID, review.MediaType, review.Rating, review.Comment).Scan(&review.ID); err != nil {
-		if err == pgx.ErrNoRows {
+		if errors.Is(err, pgx.ErrNoRows) {
 			return nil, errs.NotFound(string(review.MediaType), "id", review.MediaID)
-		} else if pgErr, ok := err.(*pgconn.PgError); ok {
-			if pgErr.Code == pgerrcode.UniqueViolation {
-				return nil, errs.Conflict("review", "(user_id, media_id)", fmt.Sprintf("(%s, %d)", review.UserID, review.MediaID))
-			}
-			if pgErr.Code == pgerrcode.ForeignKeyViolation && pgErr.ConstraintName == UserFKeyConstraint {
-				return nil, errs.NotFound("user", "id", review.UserID)
-			}
+		} else if errs.IsUniqueViolation(err, uniqueUserMediaConstraint) {
+			return nil, errs.Conflict("review", "(user_id, media_id)", fmt.Sprintf("(%s, %d)", review.UserID, review.MediaID))
+		} else if errs.IsForeignKeyViolation(err, userFKeyConstraint) {
+			return nil, errs.NotFound("user", "id", review.UserID)
 		}
+
 		return nil, err
 	}
 
