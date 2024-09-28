3a8fa006eba7531dac11a78e35015ea13cdb62da
garrettladley <garrett.ladley14@gmail.com>
2024-09-28 13:40:18 -0400

    feat: crt key in http.Client

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -3,12 +3,10 @@ package main
 import (
 	"context"
 	"log"
-	"os"
 
 	"platnm/internal/config"
 	"platnm/internal/service"
 
-	"github.com/gofiber/fiber/v2"
 	_ "github.com/lib/pq"
 	"github.com/sethvargo/go-envconfig"
 )
@@ -22,15 +20,7 @@ func main() {
 
 	app := service.InitApp(config)
 
-	if err := listner(app, config.Application.Port)(); err != nil {
+	if err := app.Listen(":" + config.Application.Port); err != nil {
 		log.Fatalf("Failed to start server: %v", err)
 	}
-}
-
-func listner(app *fiber.App, port string) func() error {
-	if os.Getenv("APP_ENVIRONMENT") != "production" {
-		return func() error { return app.Listen(":" + port) }
-	}
-
-	return func() error { return app.ListenTLS(":"+port, "server.crt", "server.key") }
 }
