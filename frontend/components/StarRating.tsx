090aea7426e08d36ca41286fa75913b0583b24e9
gaikwadsid <sidgaikwad853@gmail.com>
2024-10-05 23:21:26 -0400

    Added star rating

--- frontend/components/StarRating.tsx
+++ frontend/components/StarRating.tsx
@@ -1,32 +1,28 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { StyleSheet, View, Text, Image } from "react-native";
-import { Rating } from "react-native-ratings"
-import GreyStar from "@/components/GreyStar";
-
+import { Rating } from '@kolking/react-native-rating';
 
 export function StarRating() { 
 
     const [rating, setRating] = useState(0);
 
+    const handleChange = useCallback(
+        (value: number) => setRating(Math.round((rating + value) * 5) / 10),
+        [rating],
+    );
+
     return (
         <View style={styles.ratingContainer}>
-            <Text>Rating</Text>
-            {/* <StarRatingComp
-                rating={rating}
-                onChange={setRating}
-                color={"#555151"}
-                emptyColor={"#C4C4C4"}
-            /> */}
-
-            <Rating
-            type='custom'
-            ratingImage={GreyStar}
-            ratingColor='#555151'
-            ratingBackgroundColor='#C4C4C4'
-            ratingCount={5}
-            imageSize={33}
-            onFinishRating={this.rating}
-            style={{ paddingVertical: 10 }}
+            <Text style={styles.rateText}>Rate</Text>
+            <Rating 
+                size={27} 
+                rating={rating} 
+                onChange={handleChange} 
+                scale={1} 
+                fillColor="#555151" 
+                touchColor="#555151" 
+                baseColor="#C4C4C4;" 
+                spacing={10} 
             />
         </View>
     );
@@ -37,13 +33,13 @@ const styles = StyleSheet.create({
       backgroundColor: "#ffffff",
       fontFamily: "Roboto",
       justifyContent: "space-between",
-      flexDirection: 'row',
+      flexDirection: 'column',
       padding: 20,
       color: "#434343",
       fontSize: 19,
     },
-    stars: {
-        flexDirection: 'row',
+    rateText: {
+        marginBottom: 10,
     }
   });
   
