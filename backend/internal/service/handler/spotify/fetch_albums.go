9748f390c1f32ed1bebc1b9b35f56a091e90fc32
ddusichka <78527291+ddusichka@users.noreply.github.com>
2024-10-06 23:44:59 -0400

    split into helpers

--- backend/internal/service/handler/spotify/fetch_albums.go
+++ backend/internal/service/handler/spotify/fetch_albums.go
@@ -1,7 +1,6 @@
 package spotify
 
 import (
-	"fmt"
 	"platnm/internal/models"
 	"platnm/internal/service/ctxt"
 
@@ -20,87 +19,79 @@ type AddedContent struct {
 }
 
 func (h *SpotifyHandler) GetNewReleases(c *fiber.Ctx) error {
-	client, err := ctxt.GetSpotifyClient(c)
+	newReleases, err := fetchNewReleasesFromSpotify(c)
 	if err != nil {
 		return err
 	}
 
-	releases, err := client.NewReleases(c.Context(), spotify.Limit(20))
-	if err != nil {
-		return err
-	}
-
-	albums := []*models.Album{}
-	artists := []*models.Artist{}
-
-	fmt.Printf("releases.Albums: %v", releases.Albums[0])
-
-	for _, album := range releases.Albums {
-
-		fmt.Println("looking for album: ", album.Name)
-		albumId, err := h.MediaRepository.GetExistingAlbumBySpotifyID(c.Context(), album.ID.String())
-		if err != nil {
+	addedContent := &AddedContent{}
+	for _, album := range newReleases.Albums {
+		if err := h.handleAlbum(c, album, addedContent); err != nil {
 			return err
 		}
+	}
 
-		if albumId != nil {
-			break // don't add the album or corresponding artists if the album already exists
+	return c.Status(fiber.StatusOK).JSON(addedContent)
+}
+
+func (h *SpotifyHandler) handleAlbum(c *fiber.Ctx, album spotify.SimpleAlbum, addedContent *AddedContent) error {
+	for _, artist := range album.Artists {
+		if err := h.handleArtist(c, artist, addedContent); err != nil {
+			return err
 		}
+	}
+	albumId, err := h.MediaRepository.GetExistingAlbumBySpotifyID(c.Context(), album.ID.String())
+	if err != nil {
+		return err
+	}
 
-		newAlbum, err := h.MediaRepository.AddAlbum(c.Context(), &models.Album{
-			SpotifyID:   album.ID.String(),
-			Title:       album.Name,
-			ReleaseDate: album.ReleaseDateTime(),
-			Cover:       album.Images[0].URL,
-			Country:     album.AvailableMarkets[0],
+	if albumId != nil {
+		return nil
+	}
+
+	addedAlbum, err := h.MediaRepository.AddAlbum(c.Context(), &models.Album{
+		SpotifyID:   album.ID.String(),
+		Title:       album.Name,
+		ReleaseDate: album.ReleaseDateTime(),
+		Cover:       album.Images[0].URL,
+	})
+
+	if err != nil {
+		return err
+	}
+
+	addedContent.Albums = append(addedContent.Albums, addedAlbum)
+	return nil
+}
+
+func (h *SpotifyHandler) handleArtist(c *fiber.Ctx, artist spotify.SimpleArtist, addedContent *AddedContent) error {
+	/* TODO: it's possible that the artist can exist but not be detected by spotifyID lookup if it was
+	created through a future analogous Apple Music pathway.
+	we need more sophisticated artist search logic, but are limited by the overlap betweem the two APIs.
+	the only 100% shared data point is the artist name, which is not unique, posing a problem.
+	for now, we'll just create a new artist if one doesn't exist */
+	artistId, err := h.MediaRepository.GetExistingArtistBySpotifyID(c.Context(), artist.ID.String())
+	if err != nil {
+		return err
+	}
+	if artistId == nil {
+		newArtist, err := h.MediaRepository.AddArtist(c.Context(), &models.Artist{
+			SpotifyID: artist.ID.String(),
+			Name:      artist.Name,
 		})
 		if err != nil {
 			return err
 		}
-		albums = append(albums, newAlbum)
+		addedContent.Artists = append(addedContent.Artists, newArtist)
+	}
+	return nil
+}
 
-		if len(album.Artists) == 0 {
-			fmt.Println("album.Artists is nil or empty")
-			return fiber.NewError(fiber.StatusBadRequest, "No artists found in the album")
-		}
-
-		for _, artist := range album.Artists {
-			artistId, err := h.MediaRepository.GetExistingArtistBySpotifyID(c.Context(), artist.ID.String())
-			if err != nil {
-				return err
-			}
-
-			/* TODO: it's possible that the artist can exist but not be detected by spotifyID lookup
-			   if it was created through an analogous Apple Music pathway
-			   we need more sophisticated artist search logic, but are limited by the overlap betweem the two APIs
-			   the only 100% shared data point is the artist name, which is not unique, posing a problem.
-			   for now, we'll just create a new artist if one doesn't exist */
-
-			if artistId == nil {
-				artist, err := h.MediaRepository.AddArtist(c.Context(), &models.Artist{
-					SpotifyID: artist.ID.String(),
-					Name:      artist.Name,
-				})
-				if err != nil {
-					return err
-				}
-
-				artistId = &artist.ID
-
-				artists = append(artists, artist)
-			}
-
-			err = h.MediaRepository.AddAlbumArtist(c.Context(), newAlbum.ID, *artistId)
-			if err != nil {
-				return err
-			}
-		}
+func fetchNewReleasesFromSpotify(c *fiber.Ctx) (*spotify.SimpleAlbumPage, error) {
+	client, err := ctxt.GetSpotifyClient(c)
+	if err != nil {
+		return &spotify.SimpleAlbumPage{}, err
 	}
 
-	addedContent := &AddedContent{
-		Albums:  albums,
-		Artists: artists,
-	}
-
-	return c.Status(fiber.StatusOK).JSON(addedContent)
+	return client.NewReleases(c.Context(), spotify.Limit(20))
 }
