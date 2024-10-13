1e61af9cf951b67d6bfdf2575a0ab14091ec828e
aahil nishad <aahiltn@gmail.com>
2024-10-13 17:58:49 -0400

    last linting fix?

--- frontend/metro.config.js
+++ frontend/metro.config.js
@@ -7,12 +7,12 @@ module.exports = (() => {
 
   config.transformer = {
     ...transformer,
-    babelTransformerPath: require.resolve("react-native-svg-transformer/expo")
+    babelTransformerPath: require.resolve("react-native-svg-transformer/expo"),
   };
   config.resolver = {
     ...resolver,
     assetExts: resolver.assetExts.filter((ext) => ext !== "svg"),
-    sourceExts: [...resolver.sourceExts, "svg"]
+    sourceExts: [...resolver.sourceExts, "svg"],
   };
 
   return config;
