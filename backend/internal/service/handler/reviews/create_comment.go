b07f386d316997401ba4b4f8e845b63a109bb62a
zcroft27 <croft.z@northeastern.edu>
2024-10-14 17:26:57 -0400

    Added validation that review, user, text arguments are present

--- backend/internal/service/handler/reviews/create_comment.go
+++ backend/internal/service/handler/reviews/create_comment.go
@@ -3,6 +3,7 @@ package reviews
 import (
 	"platnm/internal/errs"
 	"platnm/internal/models"
+	"strconv"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -16,6 +17,21 @@ func (h *Handler) CreateComment(c *fiber.Ctx) error {
 	var req createComment
 	if err := c.BodyParser(&req); err != nil {
 		return errs.BadRequest("failed to parse request body")
+	}
+
+	if req.ReviewID == 0 {
+		return errs.BadRequest("review_id is required")
+	} else if req.UserID == "" {
+		return errs.BadRequest("user_id is required")
+	} else if req.Text == "" {
+		return errs.BadRequest("text is required")
+	}
+
+	// get the existing review (convert the review_id from int to string)
+	existingReview, err := h.reviewRepository.GetExistingReview(c.Context(), strconv.Itoa(req.ReviewID))
+
+	if existingReview == nil {
+		return errs.NotFound("Review", "id", req.ReviewID)
 	}
 
 	// Create the comment in the repository
