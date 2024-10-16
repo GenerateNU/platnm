c4f2fc0c271f7caa8a74aebc026d221e79781635
gaikwadsid <sidgaikwad853@gmail.com>
2024-10-15 22:18:52 -0400

    fixed lint

--- frontend/components/AuthProvider.tsx
+++ frontend/components/AuthProvider.tsx
@@ -1,45 +1,45 @@
 import React, {
-    createContext,
-    useState,
-    useContext,
-    PropsWithChildren,
-  } from "react";
-  
-  interface AuthProviderProps {
-    username: string;
-    accessToken: string;
-    updateAccessToken: (token: string) => void;
-    updateUsername: (username: string) => void;
-  }
-  
-  const AuthContext = createContext<AuthProviderProps>({
-    username: "",
-    accessToken: "",
-    updateAccessToken: () => {},
-    updateUsername: () => {},
-  });
-  
-  export const AuthProvider: React.FC<PropsWithChildren> = ({ children }) => {
-    const [username, setUsername] = useState("");
-    const [accessToken, setAccessToken] = useState("");
-  
-    const updateAccessToken = (token: string) => {
-      setAccessToken(token);
-    };
-  
-    const updateUsername = (username: string) => {
-      setUsername(username);
-    };
-  
-    return (
-      <AuthContext.Provider
-        value={{ username, accessToken, updateAccessToken, updateUsername }}
-      >
-        {children}
-      </AuthContext.Provider>
-    );
+  createContext,
+  useState,
+  useContext,
+  PropsWithChildren,
+} from "react";
+
+interface AuthProviderProps {
+  username: string;
+  accessToken: string;
+  updateAccessToken: (token: string) => void;
+  updateUsername: (username: string) => void;
+}
+
+const AuthContext = createContext<AuthProviderProps>({
+  username: "",
+  accessToken: "",
+  updateAccessToken: () => {},
+  updateUsername: () => {},
+});
+
+export const AuthProvider: React.FC<PropsWithChildren> = ({ children }) => {
+  const [username, setUsername] = useState("");
+  const [accessToken, setAccessToken] = useState("");
+
+  const updateAccessToken = (token: string) => {
+    setAccessToken(token);
   };
-  
-  export const useAuthContext = () => {
-    return useContext(AuthContext);
+
+  const updateUsername = (username: string) => {
+    setUsername(username);
   };
+
+  return (
+    <AuthContext.Provider
+      value={{ username, accessToken, updateAccessToken, updateUsername }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuthContext = () => {
+  return useContext(AuthContext);
+};
