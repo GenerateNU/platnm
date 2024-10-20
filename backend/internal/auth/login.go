6f6a80901bbe213df850612f1a1d6f338360b19d
gaikwadsid <sidgaikwad853@gmail.com>
2024-10-19 21:18:11 -0400

    fixed merge conflicts

---
+++ backend/internal/auth/login.go
@@ -0,0 +1,80 @@
+package auth
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"platnm/internal/config"
+	"platnm/internal/errs"
+)
+
+type SignInResponse struct {
+	AccessToken  string      `json:"access_token"`
+	TokenType    string      `json:"token_type"`
+	ExpiresIn    int         `json:"expires_in"`
+	RefreshToken string      `json:"refresh_token"`
+	User         interface{} `json:"user"`
+	Error        interface{} `json:"error"`
+}
+
+func GetAuthToken(cfg *config.Supabase, email string, password string) (string, error) {
+
+	supabaseURL := cfg.URL
+	apiKey := cfg.Key
+
+	// Prepare the request payload
+	payload := map[string]string{
+		"email":    email,
+		"password": password,
+	}
+	payloadBytes, err := json.Marshal(payload)
+	if err != nil {
+		return "", err
+	}
+
+	// Create the HTTP POST request
+	req, err := http.NewRequest("POST", fmt.Sprintf("%s/auth/v1/token?grant_type=password", supabaseURL), bytes.NewBuffer(payloadBytes))
+	if err != nil {
+		return "", err
+	}
+
+	// Set headers
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("apikey", apiKey)
+
+	// Execute the request
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", errs.BadRequest("failed to execute request")
+	}
+	defer resp.Body.Close()
+
+	// Read the response body
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", errs.BadRequest("failed to read response body")
+	}
+
+	// Check if the response was successful
+	if resp.StatusCode != http.StatusOK {
+		return "", errs.BadRequest(fmt.Sprintf("failed to login %d, %s", resp.StatusCode, body))
+	}
+
+	// Parse the response JSON
+	var signInResponse SignInResponse
+	err = json.Unmarshal(body, &signInResponse)
+	if err != nil {
+		return "", errs.BadRequest("failed to parse response body")
+	}
+
+	// Make sure response does not contain an error
+	if signInResponse.Error != nil {
+		return "", errs.BadRequest(fmt.Sprintf("sign in response error %v", signInResponse.Error))
+	}
+
+	// Return the access token
+	return signInResponse.AccessToken, nil
+}
