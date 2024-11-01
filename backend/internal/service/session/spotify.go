38fb26df34840a75d93807ca7f18e12d37a744ca
Zach Croft <148019409+zcroft27@users.noreply.github.com>
2024-11-01 11:52:05 -0400

    Get User's Top Items (#121)
    Co-authored-by: aaronkim218 <218adk@gmail.com>

--- backend/internal/service/session/spotify.go
+++ backend/internal/service/session/spotify.go
@@ -2,64 +2,64 @@ package session
 
 import (
 	"github.com/gofiber/fiber/v2"
-	"github.com/zmb3/spotify/v2"
+	"golang.org/x/oauth2"
 )
 
 const (
-	authSpotifyClientKey        = "authSpotifyClient"
-	clientCredsSpotifyClientKey = "clientCredsSpotifyClient"
+	authTokenKey        = "authToken"
+	clientCredsTokenKey = "clientCredsToken"
 )
 
-func (u *SessionStore) SetAuthSpotifyClient(c *fiber.Ctx, client *spotify.Client) error {
+func (u *SessionStore) SetAuthToken(c *fiber.Ctx, token *oauth2.Token) error {
 	sess, err := u.Get(c)
 	if err != nil {
 		return err
 	}
 
-	sess.Set(authSpotifyClientKey, client)
+	sess.Set(authTokenKey, token)
 	if err := sess.Save(); err != nil {
 		return err
 	}
 	return nil
 }
 
-func (u *SessionStore) GetAuthSpotifyClient(c *fiber.Ctx) (*spotify.Client, error) {
+func (u *SessionStore) GetAuthToken(c *fiber.Ctx) (*oauth2.Token, error) {
 	sess, err := u.Get(c)
 	if err != nil {
 		return nil, err
 	}
 
-	v := sess.Get(authSpotifyClientKey)
+	v := sess.Get(authTokenKey)
 	if v == nil {
 		return nil, nil
 	}
 
-	return v.(*spotify.Client), nil
+	return v.(*oauth2.Token), nil
 }
 
-func (u *SessionStore) SetClientCredsSpotifyClient(c *fiber.Ctx, client *spotify.Client) error {
+func (u *SessionStore) SetClientCredsToken(c *fiber.Ctx, token *oauth2.Token) error {
 	sess, err := u.Get(c)
 	if err != nil {
 		return err
 	}
 
-	sess.Set(clientCredsSpotifyClientKey, client)
+	sess.Set(clientCredsTokenKey, token)
 	if err := sess.Save(); err != nil {
 		return err
 	}
 	return nil
 }
 
-func (u *SessionStore) GetClientCredsSpotifyClient(c *fiber.Ctx) (*spotify.Client, error) {
+func (u *SessionStore) GetClientCredsToken(c *fiber.Ctx) (*oauth2.Token, error) {
 	sess, err := u.Get(c)
 	if err != nil {
 		return nil, err
 	}
 
-	v := sess.Get(clientCredsSpotifyClientKey)
+	v := sess.Get(clientCredsTokenKey)
 	if v == nil {
 		return nil, nil
 	}
 
-	return v.(*spotify.Client), nil
+	return v.(*oauth2.Token), nil
 }
