ff641d256b9ae1a347269e7284eeb8be48c4b004
adescoteaux1 <ally.descoteaux.0@gmail.com>
2024-11-03 14:32:48 -0500

    fixed ordering, was a missing data issue

--- backend/internal/models/feedPost.go
+++ backend/internal/models/feedPost.go
@@ -1,24 +1,23 @@
 package models
 
 import (
-	"database/sql"
 	"time"
 )
 
 type FeedPost struct {
-	ID             int            `json:"id"`
-	UserID         string         `json:"user_id"`
-	Username       string         `json:"username"`
-	ProfilePicture string         `json:"profile_picture"`
-	MediaType      MediaType      `json:"media_type"`
-	MediaID        int            `json:"media_id"`
-	Rating         int            `json:"rating"`
-	Comments       int            `json:"comments"`
-	CreatedAt      time.Time      `json:"created_at"`
-	UpdatedAt      time.Time      `json:"updated_at"`
-	Upvotes        int            `json:"upvotes"`
-	Downvotes      int            `json:"downvotes"`
-	MediaCover     sql.NullString `json:"media_cover"`
-	MediaTitle     sql.NullString `json:"media_title"`
-	MediaArtist    sql.NullString `json:"media_artist"`
+	ID             int       `json:"id"`
+	UserID         string    `json:"user_id"`
+	Username       string    `json:"username"`
+	ProfilePicture string    `json:"profile_picture"`
+	MediaType      MediaType `json:"media_type"`
+	MediaID        int       `json:"media_id"`
+	Rating         int       `json:"rating"`
+	Comments       int       `json:"comments"`
+	CreatedAt      time.Time `json:"created_at"`
+	UpdatedAt      time.Time `json:"updated_at"`
+	Upvotes        int       `json:"upvotes"`
+	Downvotes      int       `json:"downvotes"`
+	MediaCover     string    `json:"media_cover"`
+	MediaTitle     string    `json:"media_title"`
+	MediaArtist    string    `json:"media_artist"`
 }
