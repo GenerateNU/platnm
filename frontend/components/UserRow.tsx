c9c51e82b2bbdb9ea79ce24fb6d6e0ce5f151ca3
Abhik Ray <abhikaboy@gmail.com>
2024-11-07 17:34:59 -0500

    add to onqueue started

--- frontend/components/UserRow.tsx
+++ frontend/components/UserRow.tsx
@@ -1,48 +1,49 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, Image } from 'react-native';
+import React from 'react';
+import { RecommendationsCard } from '@/components/recomendations/RecommendationsScreen';
 
-type Props = {};
+type Props = {
+	recomendations: RecommendationsCard[];
+};
 
-const UserRow = (props: Props) => {
-  return (
-    <View
-      style={{
-        flexDirection: "row",
-        gap: 10,
-        backgroundColor: "#fff",
-        padding: 20,
-      }}
-    >
-      <View
-        style={{
-          width: 50,
-          height: 50,
-          backgroundColor: "#D9D9D9",
-          borderRadius: 50,
-        }}
-      />
-      <View style={{ marginVertical: "auto" }}>
-        <Text
-          style={{
-            fontSize: 16,
-            color: "#000",
-            fontWeight: "500",
-          }}
-        >
-          UserRow
-        </Text>
-        <Text
-          style={{
-            fontSize: 14,
-            color: "#393E46",
-            fontWeight: "500",
-          }}
-        >
-          19h
-        </Text>
-      </View>
-    </View>
-  );
+const UserRow = ({ recomendations }: Props) => {
+	console.log('recomendations');
+	console.log(recomendations);
+	if (recomendations.length === 0) {
+		return <View />;
+	}
+	return (
+		<View
+			style={{
+				flexDirection: 'row',
+				gap: 10,
+				backgroundColor: '#fff',
+				padding: 20,
+			}}>
+			<Image
+				source={{ uri: recomendations[0].receommender_picture }}
+				style={{ width: 50, height: 50, borderRadius: 25 }}
+			/>
+			<View style={{ marginVertical: 'auto' }}>
+				<Text
+					style={{
+						fontSize: 16,
+						color: '#000',
+						fontWeight: '500',
+					}}>
+					{recomendations[0].recomender_name}
+				</Text>
+				<Text
+					style={{
+						fontSize: 14,
+						color: '#393E46',
+						fontWeight: '500',
+					}}>
+					19h
+				</Text>
+			</View>
+		</View>
+	);
 };
 
 export default UserRow;
