9c35ca6c600a1aedeb6230046ce1977d3f6873e0
adescoteaux1 <ally.descoteaux.0@gmail.com>
2024-11-10 16:45:53 -0500

    imports

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -1,10 +1,6 @@
-import { Image, StyleSheet, View, Text, ScrollView, TouchableOpacity, Dimensions } from "react-native";
-import { router } from "expo-router";
+import { StyleSheet, View, Text, ScrollView, TouchableOpacity, Dimensions } from "react-native";
 
-import { HelloWave } from "@/components/HelloWave";
-import ParallaxScrollView from "@/components/ParallaxScrollView";
 import { ThemedText } from "@/components/ThemedText";
-import { ThemedView } from "@/components/ThemedView";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import ReviewPreview from "@/components/ReviewPreview";
