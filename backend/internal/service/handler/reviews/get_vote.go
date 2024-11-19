e406810b1f43ebb8f58ffe0e8a2b45609595a35b
adescoteaux1 <ally.descoteaux.0@gmail.com>
2024-11-19 15:23:54 -0500

    fixing Get user vote to gte rid of unneccessary checks that are causing hangs

--- backend/internal/service/handler/reviews/get_vote.go
+++ backend/internal/service/handler/reviews/get_vote.go
@@ -1,8 +1,6 @@
 package reviews
 
 import (
-	"platnm/internal/errs"
-
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -14,39 +12,6 @@ func (h *Handler) GetUserVote(c *fiber.Ctx, postType string) error {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
 			"error": "userID and postID are required",
 		})
-	}
-
-	exists, err := h.userRepository.UserExists(c.Context(), userID)
-
-	if err != nil {
-		return err
-	}
-
-	if !exists {
-		return errs.NotFound("User", "id", userID)
-	}
-
-	if postType == "review" {
-		print("review exists?")
-		reviewExists, reviewExistsErr := h.reviewRepository.ReviewExists(c.Context(), postID)
-		if reviewExistsErr != nil {
-			print("review exists")
-			return reviewExistsErr
-		}
-
-		if !reviewExists {
-			print("review exists6")
-			return errs.NotFound("Review", "id", postID)
-		}
-	} else {
-		commentExists, commentExistsErr := h.reviewRepository.CommentExists(c.Context(), postID)
-		if commentExistsErr != nil {
-			return commentExistsErr
-		}
-
-		if !commentExists {
-			return errs.NotFound("Comment", "id", postID)
-		}
 	}
 
 	vote, err := h.voteRepository.GetVoteIfExists(c.Context(), userID, postID, postType)
