c966d2ebacb419d5ce5858f730424c003999e953
adescoteaux1 <ally.descoteaux.0@gmail.com>
2024-11-19 14:57:37 -0500

    prettier

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -95,7 +95,13 @@ export default function HomeScreen() {
             feedReviews.map((review, index) => {
               return (
                 console.log("Review: ", review),
-                (<ReviewPreview key={index} preview={review} user_Id={userId} />)
+                (
+                  <ReviewPreview
+                    key={index}
+                    preview={review}
+                    user_Id={userId}
+                  />
+                )
               );
             })
           ) : (
