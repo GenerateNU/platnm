76ab57600c2edcb1be9c7326a8544ce269c9952d
ddusichka <78527291+ddusichka@users.noreply.github.com>
2024-11-24 14:34:53 -0500

    small fixes

--- frontend/components/search/SearchResults.tsx
+++ frontend/components/search/SearchResults.tsx
@@ -1,85 +1,83 @@
-  import React, { useState } from "react";
-  import { StyleSheet, View, Text, ScrollView } from "react-native";
-  import SongChip from "@/components/search/SongChip";
-  import AlbumSearchCard from "@/components/search/AlbumSearchCard";
-  import ProfileChip from "@/components/search/ProfileChip";
-  import Profiles from "@/components/search/Profiles"
-  import Filter from "@/components/search/Filter";
+import React, { useState } from "react";
+import { StyleSheet, View, Text, ScrollView } from "react-native";
+import SongChip from "@/components/search/SongChip";
+import AlbumSearchCard from "@/components/search/AlbumSearchCard";
+import ProfileChip from "@/components/search/ProfileChip";
+import Filter from "@/components/search/Filter";
 
-  interface SearchResultsProps {
-    songs: MediaResponse[];
-    albums: MediaResponse[];
-    profiles: UserProfile[];
-    isLoading: boolean;
-    filter: "all" | "songs" | "albums" | "profile";
+interface SearchResultsProps {
+  songs: MediaResponse[];
+  albums: MediaResponse[];
+  profiles: UserProfile[];
+  isLoading: boolean;
+  filter: "all" | "songs" | "albums" | "profile";
+}
+
+type FilterOption = "all" | "songs" | "albums" | "profile";
+
+const SearchResults: React.FC<SearchResultsProps> = ({
+  songs,
+  albums,
+  profiles,
+  isLoading,
+}) => {
+  if (isLoading) {
+    return <Text style={styles.loadingText}>Searching...</Text>;
   }
 
-  type FilterOption = "all" | "songs" | "albums" | "profile";
+  if (songs.length === 0 && albums.length === 0 && profiles.length == 0) {
+    return <Text style={styles.noResults}>No results found</Text>;
+  }
 
-  const SearchResults: React.FC<SearchResultsProps> = ({
-    songs,
-    albums,
-    profiles,
-    isLoading,
-  }) => {
-    if (isLoading) {
-      return <Text style={styles.loadingText}>Searching...</Text>;
-    }
+  const [selectedFilter, setSelectedFilter] = useState<FilterOption>("all");
 
-    if (songs.length === 0 && albums.length === 0 && profiles.length == 0) {
-      return <Text style={styles.noResults}>No results found</Text>;
-    }
+  const handleFilterChange = (filter: FilterOption) => {
+    setSelectedFilter(filter);
+  };
 
-    const [selectedFilter, setSelectedFilter] = useState<FilterOption>("all");
-
-    const handleFilterChange = (filter: FilterOption) => {
-      setSelectedFilter(filter);
-    };
-
-    return (
-      <View style={styles.container}>
-        <Filter
-          currentFilter={selectedFilter}
-          onFilterChange={handleFilterChange}
-        />
-        <View style={styles.resultGrid}>
-          <ScrollView style={{ flex: 1 }} contentContainerStyle={{ flexGrow: 1 }}>
+  return (
+    <View style={styles.container}>
+      <Filter
+        currentFilter={selectedFilter}
+        onFilterChange={handleFilterChange}
+      />
+      <View style={styles.resultGrid}>
+        <ScrollView style={{ flex: 1 }} contentContainerStyle={{ flexGrow: 1 }}>
           {(selectedFilter === "all" || selectedFilter === "profile") && (
-              <View style={styles.songsList}>
-                <Text style={styles.title}>Profiles</Text>
-                {profiles?.map((profile) => (
-                  <ProfileChip
-                    display_name={profile.display_name}
-                    profile_picture={profile.profile_picture}
-                    id={profile.id}
-                    key={profile.id}
+            <View style={styles.songsList}>
+              <Text style={styles.title}>Profiles</Text>
+              {profiles?.map((profile, idx) => (
+                <ProfileChip
+                  display_name={profile.display_name}
+                  profile_picture={profile.profile_picture}
+                  id={profile.id}
+                  key={idx}
+                />
+              ))}
+            </View>
+          )}
+
+          {(selectedFilter === "all" || selectedFilter === "songs") && (
+            <View style={styles.songsList}>
+              <Text style={styles.title}>Songs</Text>
+              <ScrollView>
+                {songs?.map((song, index) => (
+                  <SongChip
+                    key={index}
+                    rank={index + 1}
+                    id={song.media.id}
+                    title={song.media.title}
+                    artist_name={song.media.artist_name}
+                    cover={song.media.cover}
                   />
                 ))}
-                {/* <Profiles profiles={profiles}/> */}
-              </View>
-            )}
-            
-            {(selectedFilter === "all" || selectedFilter === "songs") && (
-              <View style={styles.songsList}>
-                <Text style={styles.title}>Songs</Text>
-                <ScrollView>
-                  {songs?.map((song, index) => (
-                    <SongChip
-                      key={index}
-                      rank={index + 1}
-                      id={song.media.id}
-                      title={song.media.title}
-                      artist_name={song.media.artist_name}
-                      cover={song.media.cover}
-                    />
-                  ))}
-                  </ScrollView>
-              </View>
-            )}
-            {(selectedFilter === "all" || selectedFilter === "albums") && (
-              <View>
-                <Text style={styles.title}>Albums</Text>
-                <View style={styles.albumsList}>
+              </ScrollView>
+            </View>
+          )}
+          {(selectedFilter === "all" || selectedFilter === "albums") && (
+            <View>
+              <Text style={styles.title}>Albums</Text>
+              <View style={styles.albumsList}>
                 {albums?.map((album, index) => (
                   <AlbumSearchCard
                     id={album.media.id}
@@ -90,66 +88,65 @@
                     cover={album.media.cover}
                   />
                 ))}
-                </View>
               </View>
-            )}
-            </ScrollView>
-        </View>
+            </View>
+          )}
+        </ScrollView>
       </View>
-              
-    );
-  };
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-title: {
-  fontSize: 24,
-  fontWeight: "bold",
-  padding: 16,
-},
-container: {
-  flex: 1,
-  paddingHorizontal: 16,
-},
-headerContainer: {
-  marginTop: 10,
-  marginBottom: 20,
-  fontSize: 16,
-  textAlign: "center",
-  fontWeight: "600",
-  color: "#000000",
-},
-section: {
-  marginTop: 20,
-},
-sectionTitle: {
-  fontSize: 20,
-  fontWeight: "600",
-  marginBottom: 10,
-  color: "#434343",
-},
-loadingText: {
-  textAlign: "center",
-  marginTop: 20,
-  color: "#666666",
-},
-resultGrid: {
-  flexDirection: "row",
-  flexWrap: "wrap",
-  gap: 10,
-},
-songsList: {
-  marginRight: 20,
-},
-albumsList: {
-width: '48%', // Slightly less than 50% to allow for spacing
-marginBottom: 16,
-paddingHorizontal: 4,
-},
-noResults: {
-  textAlign: "center",
-  marginTop: 20,
-  color: "#666666",
-},
-  });
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    padding: 16,
+  },
+  container: {
+    flex: 1,
+    paddingHorizontal: 16,
+  },
+  headerContainer: {
+    marginTop: 10,
+    marginBottom: 20,
+    fontSize: 16,
+    textAlign: "center",
+    fontWeight: "600",
+    color: "#000000",
+  },
+  section: {
+    marginTop: 20,
+  },
+  sectionTitle: {
+    fontSize: 20,
+    fontWeight: "600",
+    marginBottom: 10,
+    color: "#434343",
+  },
+  loadingText: {
+    textAlign: "center",
+    marginTop: 20,
+    color: "#666666",
+  },
+  resultGrid: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    gap: 10,
+  },
+  songsList: {
+    marginRight: 20,
+  },
+  albumsList: {
+    width: "48%", // Slightly less than 50% to allow for spacing
+    marginBottom: 16,
+    paddingHorizontal: 4,
+  },
+  noResults: {
+    textAlign: "center",
+    marginTop: 20,
+    color: "#666666",
+  },
+});
 
-  export default SearchResults;
+export default SearchResults;
