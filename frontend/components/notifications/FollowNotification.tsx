75b963a945c0223a836f970eb36a921152dc2101
Abhik Ray <abhikaboy@gmail.com>
2024-11-29 00:39:14 -0500

    lint

--- frontend/components/notifications/FollowNotification.tsx
+++ frontend/components/notifications/FollowNotification.tsx
@@ -1,15 +1,18 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import { View, Text } from "react-native";
+import React from "react";
 
 type FollowNotificationProps = {
-	tagged: string;
-	time: string;
+  tagged: string;
+  time: string;
 };
 
-export default function FollowNotification({ tagged, time }: FollowNotificationProps) {
-	return (
-		<Text style={{ marginVertical: 'auto', width: '100%' }}>
-			<Text style={{ fontWeight: 'bold' }}>{tagged}</Text> has now followed you!
-		</Text>
-	);
+export default function FollowNotification({
+  tagged,
+  time,
+}: FollowNotificationProps) {
+  return (
+    <Text style={{ marginVertical: "auto", width: "100%" }}>
+      <Text style={{ fontWeight: "bold" }}>{tagged}</Text> has now followed you!
+    </Text>
+  );
 }
