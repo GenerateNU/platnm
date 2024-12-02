14a4d9d6cf1f638c028a2b5aeac6f5f6914e2eab
aahil nishad <aahiltn@gmail.com>
2024-12-02 18:03:03 -0500

    final lint pls

--- frontend/app/Settings.tsx
+++ frontend/app/Settings.tsx
@@ -31,7 +31,7 @@ function Settings() {
           headers: {
             Authorization: accessToken,
           },
-        }
+        },
       )
       .then(() => {
         AuthContextUtils.updateUsername("");
