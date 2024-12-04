3b8402a1a1a7ce797e63edb8c94a40959d98287d
Alex Weinberger <alexcchip@gmail.com>
2024-12-03 23:13:42 -0500

    Search page v2 (#215)
    Co-authored-by: ddusichka <78527291+ddusichka@users.noreply.github.com>

--- frontend/components/search/SearchResults.tsx
+++ frontend/components/search/SearchResults.tsx
@@ -1,10 +1,16 @@
 import React, { useState } from "react";
-import { StyleSheet, View, Text, ScrollView, Dimensions } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Text,
+  ScrollView,
+  TouchableOpacity,
+} from "react-native";
 import SongChip from "@/components/search/SongChip";
-import AlbumSearchCard from "@/components/search/AlbumSearchCard";
+import SearchCard from "@/components/search/AlbumSearchCard";
 import ProfileChip from "@/components/search/ProfileChip";
 import Filter from "@/components/search/Filter";
-import { takeWhile } from "lodash";
+import AntDesign from "@expo/vector-icons/AntDesign";
 
 interface SearchResultsProps {
   songs: Media[];
@@ -28,8 +34,7 @@ const SearchResults: React.FC<SearchResultsProps> = ({
     return <Text style={styles.noResults}>No results found</Text>;
   }
 
-  const filterOptions = ["all", "songs", "albums", "profile"];
-
+  const filterOptions: FilterOption[] = ["all", "songs", "albums", "profile"];
   const [selectedFilter, setSelectedFilter] = useState<FilterOption>("all");
 
   const handleFilterChange = (filter: FilterOption) => {
@@ -45,70 +50,145 @@ const SearchResults: React.FC<SearchResultsProps> = ({
       />
       <View style={styles.resultGrid}>
         <ScrollView style={{ flex: 1 }} contentContainerStyle={{ flexGrow: 1 }}>
-          {(selectedFilter === "all" || selectedFilter === "profile") &&
-            profiles?.length > 0 && (
-              <View style={[styles.songsList, styles.twoColumnList]}>
+          {(selectedFilter === "all" || selectedFilter === "profile") && (
+            <View>
+              {selectedFilter === "all" ? (
+                <TouchableOpacity onPress={() => setSelectedFilter("profile")}>
+                  <Text style={styles.title}>
+                    Profiles <AntDesign name="right" size={18} color="black" />
+                  </Text>
+                </TouchableOpacity>
+              ) : (
                 <Text style={styles.title}>Profiles</Text>
-                {profiles?.map((profile, idx) => (
-                  <ProfileChip
-                    display_name={profile.display_name}
-                    profile_picture={profile.profile_picture}
-                    id={profile.user_id}
-                    key={idx}
-                  />
-                ))}
+              )}
+              <View style={styles.profileContainer}>
+                {profiles == null || profiles.length === 0 ? (
+                  <Text style={styles.noResults}>No profiles found</Text>
+                ) : selectedFilter === "all" ? (
+                  (console.log(profiles[0]),
+                  profiles
+                    .slice(0, 2)
+                    .map((profile, idx) => (
+                      <ProfileChip
+                        key={idx}
+                        display_name={profile.display_name}
+                        profile_picture={profile.profile_picture}
+                        id={profile.user_id}
+                      />
+                    )))
+                ) : (
+                  profiles.map((profile, idx) => (
+                    <ProfileChip
+                      key={idx}
+                      display_name={profile.display_name}
+                      profile_picture={profile.profile_picture}
+                      id={profile.user_id}
+                    />
+                  ))
+                )}
               </View>
-            )}
+            </View>
+          )}
 
           {(selectedFilter === "all" || selectedFilter === "songs") && (
-            <View style={styles.albumsList}>
-              <Text style={styles.title}>Albums</Text>
-              <View style={styles.twoColumnList}>
-                {albums.map((album, index) => (
-                  <AlbumSearchCard
-                    id={album.id}
-                    key={album.id}
-                    rank={index + 1}
-                    artist_name={album.artist_name}
-                    album_name={album.title}
-                    cover={album.cover}
-                  />
-                ))}
+            <View>
+              {selectedFilter === "all" ? (
+                <TouchableOpacity onPress={() => setSelectedFilter("songs")}>
+                  <Text style={styles.title}>
+                    Songs <AntDesign name="right" size={18} color="black" />
+                  </Text>
+                </TouchableOpacity>
+              ) : (
+                <Text style={styles.title}>Songs</Text>
+              )}
+              {songs.length === 0 ? (
+                <Text style={styles.noResults}>No songs found</Text>
+              ) : selectedFilter === "songs" ? (
+                <View
+                  style={{
+                    flexDirection: "row",
+                    flexWrap: "wrap",
+                    justifyContent: "space-between",
+                  }}
+                >
+                  {songs?.map((song, idx) => (
+                    <View style={{ width: "48%", marginBottom: 16 }}>
+                      <SearchCard
+                        rank={idx + 1}
+                        type={song.media_type}
+                        key={idx}
+                        id={song.id}
+                        artist_name={song.artist_name}
+                        album_name={song.title}
+                        cover={song.cover}
+                      />
+                    </View>
+                  ))}
+                </View>
+              ) : (
+                <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+                  <View
+                    style={{
+                      flexDirection: "row",
+                    }}
+                  >
+                    <View style={styles.gridContainer}>
+                      {songs?.slice(0, 9).map((song, idx) => (
+                        <View style={styles.gridItem}>
+                          <SongChip
+                            rank={idx + 1}
+                            key={idx}
+                            id={song.id}
+                            title={song.title}
+                            artist_name={song.artist_name}
+                            cover={song.cover}
+                          />
+                        </View>
+                      ))}
+                    </View>
+                  </View>
+                </ScrollView>
+              )}
+            </View>
+          )}
+          {(selectedFilter === "all" || selectedFilter === "albums") && (
+            <View>
+              {selectedFilter === "all" ? (
+                <TouchableOpacity onPress={() => setSelectedFilter("albums")}>
+                  <Text style={styles.title}>
+                    Albums <AntDesign name="right" size={18} color="black" />
+                  </Text>
+                </TouchableOpacity>
+              ) : (
+                <Text style={styles.title}>Albums</Text>
+              )}
+              <View
+                style={{
+                  flexDirection: "row",
+                  flexWrap: "wrap",
+                  justifyContent: "space-between",
+                }}
+              >
+                {albums.length === 0 ? (
+                  <Text style={styles.noResults}>No albums found</Text>
+                ) : (
+                  (selectedFilter === "all" ? albums.slice(0, 2) : albums)?.map(
+                    (album, idx) => (
+                      <View style={styles.albumsList}>
+                        <SearchCard
+                          rank={idx + 1}
+                          type={"album"}
+                          key={idx}
+                          id={album.id}
+                          artist_name={album.artist_name}
+                          album_name={album.title}
+                          cover={album.cover}
+                        />
+                      </View>
+                    ),
+                  )
+                )}
               </View>
-            </View>
-          )}
-          {(selectedFilter === "all" || selectedFilter === "songs") && (
-            <View style={styles.songsList}>
-              <Text style={styles.title}>Songs</Text>
-              <ScrollView>
-                {songs?.map((song, index) => (
-                  <SongChip
-                    key={index}
-                    rank={index + 1}
-                    id={song.id}
-                    title={song.title}
-                    artist_name={song.artist_name}
-                    cover={song.cover}
-                  />
-                ))}
-              </ScrollView>
-            </View>
-          )}
-          {(selectedFilter === "all" || selectedFilter === "songs") && (
-            <View style={styles.songsList}>
-              <Text style={styles.title}>Songs</Text>
-              <ScrollView>
-                {songs?.map((song, index) => (
-                  <SongChip
-                    key={index}
-                    rank={index + 1}
-                    id={song.id}
-                    title={song.title}
-                    artist_name={song.artist_name}
-                    cover={song.cover}
-                  />
-                ))}
-              </ScrollView>
             </View>
           )}
         </ScrollView>
@@ -118,42 +198,24 @@ const SearchResults: React.FC<SearchResultsProps> = ({
 };
 
 const styles = StyleSheet.create({
+  profileContainer: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "flex-start",
+  },
   title: {
     fontSize: 24,
     fontWeight: "bold",
-    padding: 16,
+    paddingVertical: 16,
   },
   container: {
     flex: 1,
-    paddingHorizontal: 16,
-  },
-  headerContainer: {
-    marginTop: 10,
-    marginBottom: 20,
-    fontSize: 16,
-    textAlign: "center",
-    fontWeight: "600",
-    color: "#000000",
-  },
-  section: {
-    marginTop: 20,
-  },
-  sectionTitle: {
-    fontSize: 20,
-    fontWeight: "600",
-    marginBottom: 10,
-    color: "#434343",
+    paddingHorizontal: 24,
   },
   loadingText: {
     textAlign: "center",
     marginTop: 20,
     color: "#666666",
-  },
-  twoColumnList: {
-    flex: 1,
-    flexDirection: "row",
-    flexWrap: "wrap",
-    justifyContent: "space-between",
   },
   resultGrid: {
     flexDirection: "row",
@@ -165,18 +227,21 @@ const styles = StyleSheet.create({
     width: "100%",
   },
   albumsList: {
-    width: "100%",
+    width: "50%",
     marginBottom: 16,
-    paddingHorizontal: 4,
   },
-  albumsList: {
-    width: "48%", // Slightly less than 50% to allow for spacing
+  gridContainer: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    width: 500,
+  },
+  gridItem: {
+    width: "33.33%",
     marginBottom: 16,
-    paddingHorizontal: 4,
   },
   noResults: {
     textAlign: "center",
-    marginTop: 20,
+    marginTop: 6,
     color: "#666666",
   },
 });
