1bbd8ddd6ed2bf872a5904ddefd6623d164ceda9
adescoteaux1 <ally.descoteaux.0@gmail.com>
2024-12-03 20:39:03 -0500

    lint

--- frontend/components/ReviewPreview.tsx
+++ frontend/components/ReviewPreview.tsx
@@ -465,7 +465,7 @@ const styles = StyleSheet.create({
     overflow: "scroll",
     boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.2)",
     borderColor: "#ddd",
-    borderWidth: .5,
+    borderWidth: 0.5,
   },
   voteButton: {
     flexDirection: "row",
