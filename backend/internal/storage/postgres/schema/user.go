3380de384df3082f0c7cd3ef95f7000f7fd68414
ddusichka <78527291+ddusichka@users.noreply.github.com>
2024-12-05 13:06:21 -0500

    prevent null pfp error

--- backend/internal/storage/postgres/schema/user.go
+++ backend/internal/storage/postgres/schema/user.go
@@ -161,7 +161,7 @@ func (r *UserRepository) CalculateScore(ctx context.Context, id uuid.UUID) (int,
 }
 
 func (r *UserRepository) CreateUser(ctx context.Context, user models.User) (models.User, error) {
-	if err := r.db.QueryRow(ctx, `INSERT INTO "user" (id, username, display_name, email) VALUES ($1, $2, $3, $4) RETURNING id`, user.ID, user.Username, user.DisplayName, user.Email).Scan(&user.ID); err != nil {
+	if err := r.db.QueryRow(ctx, `INSERT INTO "user" (id, username, display_name, email, profile_picture) VALUES ($1, $2, $3, $4, 'https://www.pngitem.com/pimgs/m/146-1468843_profile-icon-orange-png-transparent-png.png') RETURNING id`, user.ID, user.Username, user.DisplayName, user.Email).Scan(&user.ID); err != nil {
 		return models.User{}, err
 	}
 
